9137d5537e1dd413d1e7cfc6ac2d61ec4e5f206a
atto08 <atto08@naver.com>
2024-07-25 13:04:57 +0900

    상품 폴더 설계

---
+++ src/main/java/com/potado/MySelectShop/repository/FolderRepository.java
@@ -0,0 +1,7 @@
+package com.potado.MySelectShop.repository;
+
+import com.potado.MySelectShop.entity.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FolderRepository extends JpaRepository<Folder, Long> {
+}
