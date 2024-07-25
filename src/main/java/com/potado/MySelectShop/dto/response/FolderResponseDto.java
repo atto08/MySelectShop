c892e84444d5c0e1b666f5445a3359868d68e928
atto08 <atto08@naver.com>
2024-07-25 13:52:47 +0900

    폴더 생성 조회 기능 구현

---
+++ src/main/java/com/potado/MySelectShop/dto/response/FolderResponseDto.java
@@ -0,0 +1,15 @@
+package com.potado.MySelectShop.dto.response;
+
+import com.potado.MySelectShop.entity.Folder;
+import lombok.Getter;
+
+@Getter
+public class FolderResponseDto {
+    private Long id;
+    private String name;
+
+    public FolderResponseDto(Folder folder) {
+        this.id = folder.getId();
+        this.name = folder.getName();
+    }
+}
--- src/main/java/com/potado/MySelectShop/repository/FolderRepository.java
+++ src/main/java/com/potado/MySelectShop/repository/FolderRepository.java
@@ -1,7 +1,15 @@
 package com.potado.MySelectShop.repository;
 
 import com.potado.MySelectShop.entity.Folder;
+import com.potado.MySelectShop.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FolderRepository extends JpaRepository<Folder, Long> {
+
+    // select * from folder where user_id = ? and name in (?, ?, ?);
+    List<Folder> findAllByUserAndNameIn(User user, List<String> folderNames);
+
+    List<Folder> findAllByUser(User user);
 }
